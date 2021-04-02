c2a38b1ec79484413b19e919f3a7cd5d0e28779b
Meng <meng_g2001@yahoo.com>
2021-04-02 16:07:55 +1100

    First create Enum type for Operation and test case

---
+++ SiteClearningSimulation/src/main/java/simulator/OpTypeEnum.java
@@ -0,0 +1,19 @@
+package simulator;
+
+/*
+ * Enum: Operation (Item) Table
+ */
+public enum OpTypeEnum {
+	OpComm ("communication overhead"),
+    OpFuel ("fuel usage"),
+    OpUncldLd ("unclear squares"),
+	OpDesResTree ("destruciton of protected tree"),
+	OpRepDmg ("paint damagee to builldozer");
+
+    public final String value;
+    
+    private OpTypeEnum(String s) {
+        this.value = s;
+    }
+
+}
