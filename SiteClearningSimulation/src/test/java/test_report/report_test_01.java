5a80a2d810f8a0a15cbd0104828bb2a283d8c5a4
Meng <meng_g2001@yahoo.com>
2021-04-02 15:14:39 +1100

    update test cases for command.java class.

--- SiteClearningSimulation/src/test/java/test_report/report_test_01.java
+++ SiteClearningSimulation/src/test/java/test_report/report_test_01.java
@@ -1,21 +1,61 @@
 package test_report;
 
+import static org.junit.Assert.assertSame;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import simulator.command;
 import simulator.report;
 
+/*
+ * test ItemCost Table, defined in class command
+ */
+
+
 public class report_test_01 {
 
 	@Test
+	
 	public void test() {
+		//define the test obj
+		HashMap<String, Integer> output = new HashMap<>();
 		report testObj = new report();
+		output = testObj.getItemCostTable();
+
+		/*
+		 * set expect values
+		 */
+		String[] expect_item = new String[5];
+		expect_item[0]= "communication overhead";
+		expect_item[1]= "fuel usage";
+		expect_item[2]= "unclear squares";
+		expect_item[3]= "destruciton of protected tree";
+		expect_item[4]= "paint damagee to builldozer";
 		
-		testObj.printReport();
-		testObj.operationCost(testObj.getItemName()[3]);
-		testObj.printReport();
+		int[] expect_cost = new int[5];
+		expect_cost[0] = 1;
+		expect_cost[1] = 1;
+		expect_cost[2] = 3;
+		expect_cost[3] = 10;
+		expect_cost[4] = 2;
 		
-		System.out.println(testObj.getResult_cost());
+		Map<String,Integer> expecteds = new HashMap<>();
+	    expecteds.put(expect_item[0], expect_cost[0]);
+	    expecteds.put(expect_item[1], expect_cost[1]);
+	    expecteds.put(expect_item[2], expect_cost[2]);
+	    expecteds.put(expect_item[3], expect_cost[3]);
+	    expecteds.put(expect_item[4], expect_cost[4]);
+	    /**************************************/
+		
+		
+		System.out.println(output);
+		
+	    for(Map.Entry<String, Integer> entry : output.entrySet()) {
+	    	assertSame(expecteds.get(entry.getKey()), output.get(entry.getKey()));
+        }
 	}
 
 }
--- SiteClearningSimulation/src/test/java/test_report/report_test_02.java
+++ SiteClearningSimulation/src/test/java/test_report/report_test_02.java
@@ -2,19 +2,56 @@ package test_report;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import simulator.report;
+
+/*
+ * test Activity table declared in class - command
+ */
 
 public class report_test_02 {
 
 	@Test
 	public void test() {
+		//define the test obj
+		HashMap<String, Integer> output = new HashMap<>();
 		report testObj = new report();
+		output = testObj.getActivityFuelTable();
+
+		/*
+		 * set expect values
+		 */
+		String[] expect_item = new String[4];
+		expect_item[0]= "Cleaning plain land";
+		expect_item[1]= "Visting any land that has already been cleard\"";
+		expect_item[2]= "Clearning rocky land";
+		expect_item[3]= "Clearning land containg a tree";
+				
+		int[] expect_fuel = new int[4];
+		expect_fuel[0] = 1;
+		expect_fuel[1] = 1;
+		expect_fuel[2] = 2;
+		expect_fuel[3] = 2;
+
 		
-		testObj.printReport();
-		testObj.activityFuel(testObj.getActivityName()[3]);
-		testObj.printReport();
+		Map<String,Integer> expecteds = new HashMap<>();
+	    expecteds.put(expect_item[0], expect_fuel[0]);
+	    expecteds.put(expect_item[1], expect_fuel[1]);
+	    expecteds.put(expect_item[2], expect_fuel[2]);
+	    expecteds.put(expect_item[3], expect_fuel[3]);
+	   
+	    /**************************************/
+		
+		
+		System.out.println(output);
+		
+	    for(Map.Entry<String, Integer> entry : output.entrySet()) {
+	    	assertSame(expecteds.get(entry.getKey()), output.get(entry.getKey()));
+        }
 	}
 
 }
