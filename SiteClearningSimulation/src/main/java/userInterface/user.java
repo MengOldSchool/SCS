a95ae8212b020e5229ead5229372a5a7fd2d64d0
meng <meng@My-Ubuntu>
2021-04-04 01:36:29 +1100

    first create: user class and its test case

--- SiteClearningSimulation/src/main/java/userInterface/user.java
+++ SiteClearningSimulation/src/main/java/userInterface/user.java
@@ -1,13 +1,25 @@
 package userInterface;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import systemEnum.CmdType;
+
+/*
+ * @Meng
+ * user interface: 
+ * 1) read the user input and return the status 
+ * 2) return the command 
+ * 3) store all the inputs and print out 
+ */
 
 public class user {
 	
 	private Scanner in = new Scanner(System.in);
 	private String input;
-	
-	private final int MAX_CMD_SIZE = 2;
+	private List<String> userCmd;
+	private String validCmd;
 	
 	/*
 	 * constructor
@@ -15,50 +27,84 @@ public class user {
 	
 	
 	public user() {
-		
-		
+		userCmd = new ArrayList<String>();
 	}
 	
 	/*
 	 * read user input command
 	 */
-	public void readInput() {
+	public boolean readInput() {
 		// Using Scanner for Getting Input from User
+		boolean isInputValid = true;
       
 		System.out.print("(l)eft, (r)right, (a)dvance <n>, (q)uit: ");	  
 		this.input = in.nextLine();
-		checkInput();
-        System.out.println("You entered string " + this.input);
-  
-        //int a = in.nextInt();
-        //System.out.println("You entered integer " + a);
-  
-        //float b = in.nextFloat();
-        //System.out.println("You entered float " + b);
+		isInputValid = checkInput();
         
-        
+        return isInputValid;               
 	}
 	
 	public void endInput() {
 		in.close();
 	}
 	
-	private void checkInput() {
+	
+	private boolean checkInput() {
 		char[] ch = this.input.toCharArray();
-		int cmd_lengh = ch.length; 
+		int cmd_length = ch.length; 
 		
-		if (cmd_lengh == 1) {
-			//if (ch[0] != (char)(CmdType.l)) {
-				
-			//}
+		if (cmd_length == 1) {
+			//if input is one char, check it is "l", "r", "q"
+			if (ch[0] == (CmdType.Left.value)) {
+				//store the commmand
+				userCmd.add(input);
+				validCmd = input;
+				return true;
+			}
+			else if (ch[0] == CmdType.Right.value) {
+				userCmd.add(input);
+				validCmd = input;
+				return true;
+			}
+			else if (ch[0] == CmdType.Quit.value) {
+				userCmd.add(input);
+				validCmd = input;
+				return true;
+			}
+			else {
+				//invalid command
+				return false;
+			}
 		}
-		if (ch.length <= MAX_CMD_SIZE) {
-			System.out.println(input);
-		}
-		else {
-			System.out.printf("wrong command");
+		else{
+			//if it is longer then 1 char
+			if (ch[0] == (CmdType.Adv.value)) {
+				//this first one must be "a"
+				//And, the reset must be number
+				for (int i=1; i<cmd_length; i++) {
+					if (Character.isDigit(input.charAt(i)) == false) {
+						//not digital, invalid command
+						return false;
+					}
+				}
+				userCmd.add(input);
+				validCmd = input;
+				return true;
+			}
+			else {
+				//invalid command
+				return false;
+			}
 		}
 	}
+
+	public void showAllCmd() {
+		System.out.println(userCmd);
+	}
 	
+	public String curValidCmd() {
+		return validCmd;
+	}
+		
 
 }
